fef7cadc6456e388d909d91bf5dd721965d7eefb
azeezaliyu707-ux <azeezaliyu707@gmail.com>
2026-07-06 19:25:18 +0100

    Update config.py

--- config.py
+++ config.py
@@ -10,18 +10,19 @@ class Config:
     # OpenAI API Key
     OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
     
-    # Football API (we'll use API-Football from rapidapi)
-    # You can get free API key from: https://rapidapi.com/api-sports/api/api-football/
-    FOOTBALL_API_KEY = os.getenv('FOOTBALL_API_KEY')
-    FOOTBALL_API_HOST = 'api-football-v1.p.rapidapi.com'
-    FOOTBALL_API_URL = 'https://api-football-v1.p.rapidapi.com/v3'
-    
     # Default leagues to track
     DEFAULT_LEAGUES = [
-        {'id': 39, 'name': 'Premier League'},
-        {'id': 140, 'name': 'La Liga'},
-        {'id': 135, 'name': 'Serie A'},
-        {'id': 78, 'name': 'Bundesliga'},
-        {'id': 61, 'name': 'Ligue 1'},
-        {'id': 2, 'name': 'Champions League'}
+        {'name': 'Premier League', 'country': 'England'},
+        {'name': 'La Liga', 'country': 'Spain'},
+        {'name': 'Serie A', 'country': 'Italy'},
+        {'name': 'Bundesliga', 'country': 'Germany'},
+        {'name': 'Ligue 1', 'country': 'France'},
+        {'name': 'UEFA Champions League', 'country': 'Europe'}
+    ]
+    
+    # Top teams for quick access
+    TOP_TEAMS = [
+        'Manchester City', 'Real Madrid', 'Bayern Munich', 'PSG', 
+        'Liverpool', 'Barcelona', 'AC Milan', 'Arsenal',
+        'Chelsea', 'Manchester United', 'Juventus', 'Inter Milan'
     ]
